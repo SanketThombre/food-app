66ba02fcd807794f9670550964941a1212c0f324
sanket008 <sanket.p.thombare@gmail.com>
2022-05-30 18:15:30 +0530

    food app

---
+++ scripts/navbar.js
@@ -0,0 +1,15 @@
+function navbar() {
+     
+    return `<div id="navbar">
+    <h3><a href="search_recipe.html">Home</a></h3>
+
+    <div id="options">
+      <h3><a href="recipe_of_day.html">Recipe Of Day</a></h3>
+      <h3><a href="latest.html">Latest Recipe</a></h3>
+      <h3><a href="login.html">Sign In</a></h3>
+      <h3><a href="signup.html">Sign Up</a></h3>
+    </div>
+  </div>`
+}
+ 
+export default navbar;
---
+++ scripts/show_menu.js
@@ -0,0 +1,36 @@
+async function getData(url) {
+    
+    try {
+        let res = await fetch(url)
+
+        let data = await res.json();
+        return data
+    }
+    catch (er) {
+        console.log("er:",er);
+    }
+}
+
+
+function appendData(data,location) {
+    
+    data.forEach(({ title, image }) => { 
+
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+
+        p.innerHTML = title;
+
+        let img = document.createElement("img");
+
+        img.src = image;
+
+        div.append(img, p);
+
+        location.append(div);
+
+    });
+
+}
+
+export {getData,appendData}
